9d4ba6f8a78b3ecb42f016a89233c68d9edcad12
Vincenzo D'Amore <v.damore@gmail.com>
2015-11-03 12:13:38 +0100

    using whitespace tokenizer before synonyms added few log lines during init and resource load

--- src/main/java/it/apache/solr/search/SynonymsEdismaxQParserPlugin.java
+++ src/main/java/it/apache/solr/search/SynonymsEdismaxQParserPlugin.java
@@ -6,11 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.core.KeywordTokenizer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
 import org.apache.lucene.analysis.synonym.SynonymFilterFactory;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.util.ResourceLoader;
 import org.apache.lucene.analysis.util.ResourceLoaderAware;
+import org.apache.lucene.util.Version;
 import org.apache.solr.common.SolrException;
 import org.apache.solr.common.params.ModifiableSolrParams;
 import org.apache.solr.common.params.SolrParams;
@@ -56,7 +57,10 @@ public class SynonymsEdismaxQParserPlugin extends ExtendedDismaxQParserPlugin im
 			if (tokenizerFactory == null)
 				tokenizerFactory = "solr.WhitespaceTokenizerFactory";
 
-			logger.info("synonyms =" + synonyms);
+			logger.info("synonyms= " + synonyms);
+			logger.info("ignoreCase= " + ignoreCase);
+			logger.info("expand= " + expand);
+			logger.info("tokenizerFactory= " + tokenizerFactory);
 
 			Map<String, String> args = new HashMap<>();
 			args.put("synonyms", synonyms);
@@ -65,11 +69,14 @@ public class SynonymsEdismaxQParserPlugin extends ExtendedDismaxQParserPlugin im
 			args.put("expand", Boolean.toString(expand));
 			args.put("tokenizerFactory", tokenizerFactory);
 			try {
+				logger.info("creating new SynonymFilterFactory");
 				syf = new SynonymFilterFactory(args);
+				logger.info("created new SynonymFilterFactory");
 			} catch (SolrException e) {
 				logger.error("Error creating SynonymFilterFactory "+ e.getMessage());
 				syf = null;
 			}
+			logger.info("init complete.");
 		}
 
 	}
@@ -80,6 +87,7 @@ public class SynonymsEdismaxQParserPlugin extends ExtendedDismaxQParserPlugin im
 		ModifiableSolrParams modparams = new ModifiableSolrParams(params);
 		String modQ = null;
 		try {
+			logger.debug("evaluating: "+qStr);
 			modQ = applySynonyms(qStr);
 			if (!modQ.equals(qStr))
 				logger.info(qStr + " -> " + modQ);
@@ -93,10 +101,12 @@ public class SynonymsEdismaxQParserPlugin extends ExtendedDismaxQParserPlugin im
 	}
 
 	private String applySynonyms(String input) throws IOException {
-		if (syf == null)
+		if (syf == null) {
+			logger.warn("syf is null exiting!!!");
 			return input;
+		}
 		StringBuffer strbuf = new StringBuffer();
-		try (KeywordTokenizer wt = new KeywordTokenizer(new StringReader(input))) {
+		try (WhitespaceTokenizer wt = new WhitespaceTokenizer(Version.LUCENE_48, new StringReader(input))) {
 			try (TokenStream syn = syf.create(wt)) {
 				CharTermAttribute term = syn.addAttribute(CharTermAttribute.class);
 				syn.reset();
@@ -117,10 +127,14 @@ public class SynonymsEdismaxQParserPlugin extends ExtendedDismaxQParserPlugin im
 		this.loader = loader;
 		if (synonyms != null) {
 			try {
-				if (syf != null)
+				if (syf != null) {
+					logger.debug("loading synonyms " + synonyms);
 					syf.inform(this.loader);
+				} else {
+					logger.warn("syf = null, unable to load synonyms...");
+				}
 			} catch (IOException e) {
-				logger.error("Failed to create parser " + this.getClass().getName());
+				logger.error("Failed to create parser " + e.getMessage());
 				syf = null;
 			}
 		}
